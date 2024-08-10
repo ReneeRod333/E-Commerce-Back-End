2e8f5e21a17c16ff906587c597a8afb9a57f1e62
Renee Rodriguez <poulin.reneemarie@gmail.com>
2024-08-10 16:45:48 -0400

    Set database connection and models.

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,33 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+  ? new Sequelize(process.env.DB_URL, {
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
       },
-    });
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
+
+// Test the database connection
+sequelize
+  .authenticate()
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch((error) => console.error('Unable to connect to the database:', error));
 
 module.exports = sequelize;
